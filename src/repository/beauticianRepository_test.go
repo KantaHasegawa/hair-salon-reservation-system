a35bf8d4f515afa3a3744ce93aa2abe3adedbde6
KantaHasegawa <gyoza20000@gmail.com>
2022-04-17 18:15:17 +0900

    reafactor: Refactoring Seed data

--- src/repository/beauticianRepository_test.go
+++ src/repository/beauticianRepository_test.go
@@ -4,11 +4,12 @@ import (
 	"testing"
 
 	"github.com/kantahasegawa/hair-salon-reservation-system/src/database"
+	"github.com/kantahasegawa/hair-salon-reservation-system/test/seed/beautician"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestAll(t *testing.T) {
 	r := NewBeauticianRepository(database.NewTestDatabaseHandler())
 	result, _ := r.All()
-	assert.Equal(t, 3, len(result))
+	assert.Equal(t, len(beautician.Seed), len(result))
 }
--- test/seed/beautician/beauticianFactory.go
+++ test/seed/beautician/beauticianFactory.go
@@ -9,14 +9,15 @@ import (
 	"github.com/kantahasegawa/hair-salon-reservation-system/src/entity"
 )
 
+var Seed = []entity.Beautician{
+	{Id: "1", Name: "One", Sex: "M", Price: 1},
+	{Id: "2", Name: "Two", Sex: "F", Price: 2},
+	{Id: "3", Name: "Three", Sex: "M", Price: 3},
+}
+
 func Factory(db *gorm.DB) error {
-	var seed = []entity.Beautician{
-		{Id: "1", Name: "One", Sex: "M", Price: 1},
-		{Id: "2", Name: "Two", Sex: "F", Price: 2},
-		{Id: "3", Name: "Three", Sex: "M", Price: 3},
-	}
 	var err error
-	for _, value := range seed {
+	for _, value := range Seed {
 		err = db.Create(&value).Error
 	}
 	if err != nil {
